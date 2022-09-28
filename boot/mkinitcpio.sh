18b241344e1bd713c6edb7fa391a240140ba046f
Ganreshnu <ganreshnu@gmail.com>
2022-09-27 23:41:13 -0500

    got the initramfs worked out

--- boot/mkinitcpio.sh
+++ boot/mkinitcpio.sh
@@ -3,11 +3,10 @@ here=$(dirname $BASH_SOURCE)
 
 cmdline=$(mktemp)
 #echo "i915.fastboot=1 quiet consoleblank=60 acpi_backlight=vendor" > $cmdline
-echo "quiet consoleblank=60 acpi_backlight=vendor" > $cmdline
-mkinitcpio --config $here/mkinitcpio-systemd.conf --splash /boot/splash-arch.bmp --cmdline $cmdline --uefi /boot/EFI/Linux/arch-systemd.efi --microcode /boot/intel-ucode.img
+echo "root=PARTLABEL=archlinux resume=PARTLABEL=swap i915.fastboot=1 quiet consoleblank=60 acpi_backlight=vendor" > $cmdline
+mkinitcpio --config $here/mkinitcpio-systemd.conf --splash /usr/share/systemd/bootctl/splash-arch.bmp --cmdline $cmdline --uefi /boot/EFI/Linux/arch-systemd.efi --microcode /boot/intel-ucode.img
 rm $cmdline
 
-install -C /usr/share/systemd/bootctl/splash-arch.bmp /boot/
 install -C $here/loader/loader.conf /boot/loader/
 install -C $here/loader/entries/*.conf /boot/loader/entries/
 
