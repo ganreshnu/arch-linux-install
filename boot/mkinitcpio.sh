1386f7f8e9b4ec571c24dcf931495c410351d2ea
Ganreshnu <ganreshnu@gmail.com>
2022-09-29 07:59:22 -0500

    modernized

--- boot/mkinitcpio.sh
+++ boot/mkinitcpio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function showhelp() {
+showhelp() {
 	cat <<EOD
 Usage: mkinitcpio.sh [OPTIONS] ROOT_PARTITION
 
@@ -17,104 +17,109 @@ Options:
 EOD
 }
 
-opts=()
-modules=()
-esp_path=/boot
-wanthelp=0
-while :
-do
-	if [[ "$1" == --* ]]; then
-		case "$1" in
-			--help )
-				wanthelp=1
-				shift
-				;;
-			--cmdline )
-				showcmdline=yes
-				shift
-				;;
-			--microcode )
-			 	microcode=$2
-				shift 2
-				;;
-			--module )
-			 	modules+=("$2")
-				shift 2
-				;;
-			--resume )
-			 	resume=$2
-				shift 2
-				;;
-			--opt )
-			 	opts+=("$2")
-				shift 2
-				;;
-			--esp-path )
-				esp_path=$2
-				shift 2
-				;;
-			--reboot )
-				reboot=yes
-				shift
-				;;
-			* )
-				>&2 echo "unknown option $1"
-				wanthelp=2
-				shift
-				;;
-		esac
-	else
-		break
+main() {
+	local opts=()
+	local modules=()
+	local esp_path=/boot
+	local showcmdline
+	local microcode
+	local wanthelp=0
+	local resume
+	local reboot
+	while :
+	do
+		if [[ "$1" == --* ]]; then
+			case "$1" in
+				--help )
+					wanthelp=1
+					shift
+					;;
+				--cmdline )
+					showcmdline=yes
+					shift
+					;;
+				--microcode )
+				 	microcode="$2"
+					shift 2
+					;;
+				--module )
+				 	modules+=("$2")
+					shift 2
+					;;
+				--resume )
+				 	resume="$2"
+					shift 2
+					;;
+				--opt )
+				 	opts+=("$2")
+					shift 2
+					;;
+				--esp-path )
+					esp_path="$2"
+					shift 2
+					;;
+				--reboot )
+					reboot=yes
+					shift
+					;;
+				* )
+					>&2 echo "unknown option $1"
+					wanthelp=2
+					shift
+					;;
+			esac
+		else
+			break
+		fi
+	done
+	
+	if [[ ! $1 ]]; then
+	 	>&2 echo "ROOT_PARTITION must be set"
+		wanthelp=2
 	fi
-done
-
-if [ ! "$1" ]; then
- 	>&2 echo "ROOT_PARTITION must be set"
-	wanthelp=2
-fi
-
-[ $wanthelp -eq 1 ] && showhelp && exit
-[ $wanthelp -eq 2 ] && showhelp && exit 1
-
-# directory containing this script
-here=$(dirname $BASH_SOURCE)
-
-# generate command line
-cmdline_file=$(mktemp)
-cmdline="root=$1 quiet consoleblank=60"
-[ $resume ] && cmdline="$cmdline resume=$resume"
-for i in ${opts[@]}; do
-	cmdline="$cmdline $i"
-done
-echo $cmdline > $cmdline_file
-
-if [ $showcmdline ]; then
-	cat $cmdline_file
-	rm $cmdline_file
-	exit
-fi
-
-# setup the microcode argument
-[ $microcode ] && microcode="--microcode $microcode"
-
-# generate config file
-for i in ${modules[@]}; do
-	mods="$mods $i"
-done
-config_file=$(mktemp)
-cat > $config_file <<EOD
-MODULES=($mods)
-HOOKS=(keyboard autodetect systemd modconf block filesystems fsck)
+	
+	[[ $wanthelp -eq 1 ]] && showhelp && exit
+	[[ $wanthelp -eq 2 ]] && showhelp && exit 1
+	
+	# directory containing this script
+	local here=$(dirname $BASH_SOURCE)
+	
+	# generate command line
+	local cmdline_file=$(mktemp)
+	local cmdline="root=$1 quiet consoleblank=60"
+	[[ $resume ]] && cmdline="$cmdline resume=$resume"
+	for i in ${opts[@]}; do
+		cmdline="$cmdline $i"
+	done
+	echo $cmdline > $cmdline_file
+	
+	if [[ $showcmdline ]]; then
+		cat $cmdline_file
+		rm $cmdline_file
+		exit
+	fi
+	
+	# setup the microcode argument
+	[[ $microcode ]] && microcode="--microcode $microcode"
+	
+	# generate config file
+	local mods
+	for i in ${modules[@]}; do
+		mods="$mods $i"
+	done
+	local config_file=$(mktemp)
+	cat > $config_file <<-EOD
+	MODULES=($mods)
+	HOOKS=(keyboard autodetect systemd modconf block filesystems fsck)
 EOD
-mkinitcpio --config $config_file --splash /usr/share/systemd/bootctl/splash-arch.bmp \
-	--cmdline $cmdline_file --uefi $esp_path/EFI/Linux/archlinux-systemd.efi $microcode
+	mkinitcpio --config $config_file --splash /usr/share/systemd/bootctl/splash-arch.bmp \
+		--cmdline $cmdline_file --uefi $esp_path/EFI/Linux/archlinux-systemd.efi $microcode
 
-#mkinitcpio --config $here/mkinitcpio-systemd.conf --splash /usr/share/systemd/bootctl/splash-arch.bmp --cmdline $cmdline_file --uefi /boot/EFI/Linux/arch-systemd.efi --microcode /boot/intel-ucode.img
-rm $cmdline_file
-rm $config_file
-
-[[ $reboot ]] && reboot now
-#echo "i915.fastboot=1 quiet consoleblank=60 acpi_backlight=vendor" > $cmdline
-#echo "root=PARTLABEL=archlinux resume=PARTLABEL=swap i915.fastboot=1 quiet consoleblank=60 acpi_backlight=vendor" > $cmdline
+	rm $cmdline_file
+	rm $config_file
+	
+	[[ $reboot ]] && reboot now
+}
+main "$@"
 
 # vim: ts=3 sw=1 sts=0
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -eo pipefail
 
-function showhelp() {
+showhelp() {
 	cat <<EOD
 Usage: install.sh HOSTNAME
 
@@ -8,207 +8,204 @@ Install an Arch Linux Distribution.
 EOD
 }
 
-isvm=$(dmesg |grep "Hypervisor detected")
-[ "$isvm" ] && isvm=yes
-
-wanthelp=0
-while :
-do
-	if [[ $1 == --* ]]; then
-		case "$1" in
-			--help )
-				wanthelp=1
-				shift
-				;;
-			* )
-				>&2 echo "unknown option: $1"
-				wanthelp=2
-				shift
-				;;
-		esac
-	else
-		break
+main() {
+	local isvm=$(dmesg |grep "Hypervisor detected")
+	[[ $isvm ]] && isvm=yes
+	
+	local wanthelp=0
+	while :
+	do
+		if [[ $1 == --* ]]; then
+			case "$1" in
+				--help )
+					wanthelp=1
+					shift
+					;;
+				* )
+					>&2 echo "unknown option: $1"
+					wanthelp=2
+					shift
+					;;
+			esac
+		else
+			break
+		fi
+	done
+	
+	[[ $wanthelp -eq 1 ]] && showhelp && exit
+	
+	local hostname=$1
+	if [[ ! $hostname ]]; then
+	 	>&2 echo "must declare hostname"
+		wanthelp=2
 	fi
-done
-
-[[ $wanthelp -eq 1 ]] && showhelp && exit
-
-hostname=$1
-if [[ ! $hostname ]]; then
- 	>&2 echo "must declare hostname"
-	wanthelp=2
-fi
-
-[[ $wanthelp -eq 2 ]] && showhelp && exit 1
-
-set -e
-here=$(dirname $BASH_SOURCE)
-
-timedatectl set-ntp true
-
-if [[ $isvm ]]; then
-	mkfs.ext4 /dev/sda3
-	mount /dev/sda3 /mnt
-	filesystem=ext4
-else
-	mkfs.f2fs -f -l root -O extra_attr,inode_checksum,sb_checksum,compression,encrypt /dev/sda3
-	mount -o compress_algorithm=zstd:6,compress_chksum,gc_merge,lazytime /dev/sda3 /mnt
-	filsystem=f2fs
-fi
-
-mkfs.fat -F 32 /dev/sda1
-mount --mkdir /dev/sda1 /mnt/boot
-
-mkswap /dev/sda2
-swapon /dev/sda2
-
-if [[ $isvm ]]; then
-	firmware="e2fsprogs"
-else
-	firmware="linux-firmware intel-ucode broadcom-wl iwd f2fs-tools"
-fi
-
-# bootstrap the install with the base packages
-pacstrap -i /mnt linux mkinitcpio $firmware \
-	base efibootmgr dosfstools btrfs-progs \
-	iptables-nft firewalld polkit \
-	bash-completion man-db man-pages texinfo \
-	tpm2-tss libfido2 sudo openssh \
-	git arch-install-scripts vim
-
-
-# generate the fstab -- compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# enable the required services
-[[ ! $isvm ]] && arch-chroot /mnt systemctl enable iwd.service
-
-arch-chroot /mnt /bin/bash <<EOD
-systemctl enable systemd-networkd.service
-systemctl enable systemd-resolved.service
-systemctl enable firewalld.service
+	
+	[[ $wanthelp -eq 2 ]] && showhelp && exit 1
+	
+	local here=$(dirname $BASH_SOURCE)
+	
+	timedatectl set-ntp true
+	
+	local filesystem
+	if [[ $isvm ]]; then
+		mkfs.ext4 /dev/sda3
+		mount /dev/sda3 /mnt
+		filesystem=ext4
+	else
+		mkfs.f2fs -f -l root -O extra_attr,inode_checksum,sb_checksum,compression,encrypt /dev/sda3
+		mount -o compress_algorithm=zstd:6,compress_chksum,gc_merge,lazytime /dev/sda3 /mnt
+		filsystem=f2fs
+	fi
+	
+	mkfs.fat -F 32 /dev/sda1
+	mount --mkdir /dev/sda1 /mnt/boot
+	
+	mkswap /dev/sda2
+	swapon /dev/sda2
+	
+	local firmware
+	if [[ $isvm ]]; then
+		firmware="e2fsprogs"
+	else
+		firmware="linux-firmware intel-ucode broadcom-wl iwd f2fs-tools"
+	fi
+	
+	# bootstrap the install with the base packages
+	pacstrap -i /mnt linux mkinitcpio $firmware \
+		base dosfstools btrfs-progs \
+		iptables-nft firewalld polkit \
+		bash-completion man-db man-pages texinfo \
+		tpm2-tss libfido2 sudo openssh \
+		git arch-install-scripts vim
+	
+	
+	# generate the fstab -- compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime
+	genfstab -U /mnt >> /mnt/etc/fstab
+	
+	# enable the required services
+	[[ ! $isvm ]] && arch-chroot /mnt systemctl enable iwd.service
+	
+	arch-chroot /mnt /bin/bash <<-EOD
+	systemctl enable systemd-networkd.service
+	systemctl enable systemd-resolved.service
+	ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+	systemctl enable firewalld.service
 EOD
-
-# setup the hw clock
-arch-chroot /mnt hwclock --systohc
-
-
-# uncomment language from /mnt/etc/locale.gen
-sed -i \
-	-e "/en_US.UTF-8/s/^#//g" \
-	/mnt/etc/locale.gen
-
-# generate the language files
-arch-chroot /mnt locale-gen
-
-#arch-chroot /mnt /bin/bash <<EOD
-#ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
-#hwclock --systohc
-#EOD
-
-# set the lang environment variable
-#echo "LANG=$LANG" > /mnt/etc/locale.conf
-
-# set the hostname
-#echo $hostname > /mnt/etc/hostname
-
-rm /mnt/etc/machine-id
-mkdir -p /mnt/etc/systemd/system/systemd-firstboot.service.d
-cat > /mnt/etc/systemd/system/systemd-firstboot.service.d/override.conf <<EOD
-[Service]
-ExecStart=/usr/bin/systemd-firstboot --prompt
-
-[Install]
-WantedBy=sysinit.target
+	
+	# setup the hw clock
+	arch-chroot /mnt hwclock --systohc
+	
+	
+	# uncomment language from /mnt/etc/locale.gen
+	sed -i \
+		-e "/en_US.UTF-8/s/^#//g" \
+		/mnt/etc/locale.gen
+	
+	# generate the language files
+	arch-chroot /mnt locale-gen
+	
+	rm /mnt/etc/machine-id
+	mkdir -p /mnt/etc/systemd/system/systemd-firstboot.service.d
+	cat > /mnt/etc/systemd/system/systemd-firstboot.service.d/override.conf <<-EOD
+	[Service]
+	ExecStart=/usr/bin/systemd-firstboot --prompt-locale --prompt-keymap --prompt-timezone --prompt-hostname
+	
+	[Install]
+	WantedBy=sysinit.target
 EOD
-arch-chroot /mnt systemctl enable systemd-firstboot.service
-
-# enable wheel group in sudoers
-awk '/wheel/ && /NOPASSWD/' /mnt/etc/sudoers | cut -c3- > /mnt/etc/sudoers.d/wheel
-# copy the nopassword policykit config
-cp $here/etc/polkit-1/rules.d/* /mnt/etc/polkit-1/rules.d/
-
-# copy the bash profile scripts
-cp $here/etc/profile.d/* /mnt/etc/profile.d/
-
-
-# make the xdg config dir in skel
-mkdir /mnt/etc/skel/.config
-# copy the systemd user environment config files
-cp -r $here/dot-config/environment.d /mnt/etc/skel/.config/
-
-# install the gnupg config
-git -C /mnt/etc/skel/.config clone --quiet https://github.com/ganreshnu/config-gnupg.git gnupg
-chmod go-rwx /mnt/etc/skel/.config/gnupg
-cat >> /mnt/etc/skel/.bashrc <<'EOD'
-
-# BEGIN set by install.sh
-. $GNUPGHOME/.rc
-set -o vi
-# END set by install.sh
+	arch-chroot /mnt systemctl enable systemd-firstboot.service
+	
+	# enable wheel group in sudoers
+	awk '/wheel/ && /NOPASSWD/' /mnt/etc/sudoers | cut -c3- > /mnt/etc/sudoers.d/wheel
+	# copy the nopassword policykit config
+	cp $here/etc/polkit-1/rules.d/* /mnt/etc/polkit-1/rules.d/
+	
+	# copy the bash profile scripts
+	cp $here/etc/profile.d/* /mnt/etc/profile.d/
+	
+	
+	# make the xdg config dir in skel
+	mkdir /mnt/etc/skel/.config
+	# copy the systemd user environment config files
+	cp -r $here/dot-config/environment.d /mnt/etc/skel/.config/
+	
+	# install the gnupg config
+	git -C /mnt/etc/skel/.config clone --quiet https://github.com/ganreshnu/config-gnupg.git gnupg
+	chmod go-rwx /mnt/etc/skel/.config/gnupg
+	cat >> /mnt/etc/skel/.bashrc <<-'EOD'
+	
+	# BEGIN set by install.sh
+	. $GNUPGHOME/.rc
+	set -o vi
+	# END set by install.sh
 EOD
-
-# install the ssh config
-git -C /mnt/etc/skel clone --quiet https://github.com/ganreshnu/config-openssh.git .ssh
-ssh-keyscan github.com > /mnt/etc/skel/.ssh/known_hosts
-
-# install this repo
-git -C /mnt/root clone --quiet https://github.com/ganreshnu/arch-linux-install.git
-
-# setup the bootloader
-bootctl --esp-path=/mnt/boot install
-
-[[ ! $isvm ]] && fallbackopts="i915.fastboot=1 acpi_backlight=vendor"
-cat > /mnt/boot/loader/entries/fallback.conf <<EOD
-title		Arch Linux (fallback)
-linux		/vmlinuz-linux
-initrd	/initramfs-linux.img
-options	root=PARTLABEL=archlinux resume=PARTLABEL=swap
-options	rw quiet consoleblank=60 $fallbackopts
+	
+	# install the ssh config
+	git -C /mnt/etc/skel clone --quiet https://github.com/ganreshnu/config-openssh.git .ssh
+	ssh-keyscan github.com > /mnt/etc/skel/.ssh/known_hosts
+	
+	# install this repo
+	git -C /mnt/root clone --quiet https://github.com/ganreshnu/arch-linux-install.git
+	
+	# setup the bootloader
+	bootctl --esp-path=/mnt/boot install
+	
+	local fallbackopts
+	[[ ! $isvm ]] && fallbackopts="i915.fastboot=1 acpi_backlight=vendor"
+	cat > /mnt/boot/loader/entries/fallback.conf <<-EOD
+	title		Arch Linux (fallback)
+	linux		/vmlinuz-linux
+	initrd	/initramfs-linux.img
+	options	root=PARTLABEL=archlinux resume=PARTLABEL=swap
+	options	rw quiet consoleblank=60 $fallbackopts
 EOD
-
-#cp $here/boot/loader/entries/fallback.conf /mnt/boot/loader/entries/
-[[ ! $isvm ]] && microcode="--microcode /mnt/boot/intel-ucode.img"
-[[ ! $isvm ]] && opts="--opt i915.fastboot=1 --opt acpi_backlight=vendor"
-
-cat > /mnt/etc/systemd/system/mkunifiedimage.service <<EOD
-[Unit]
-Description=Make unified kernel image
-After=local-fs.target
-ConditionPathExists=!/boot/EFI/Linux/archlinux-systemd.efi
-
-[Service]
-Type=oneshot
-RemainAfterExit=true
-ExecStart=-/root/arch-linux-install/boot/mkinitcpio.sh --reboot --resume PARTLABEL=swap $microcode $opts PARTLABEL=archlinux
-StandardOutput=inherit
-StandardError=journal+console
-
-[Install]
-WantedBy=graphical.target
+	
+	#cp $here/boot/loader/entries/fallback.conf /mnt/boot/loader/entries/
+	local microcode
+	[[ ! $isvm ]] && microcode="--microcode /mnt/boot/intel-ucode.img"
+	local opts
+	[[ ! $isvm ]] && opts="--opt i915.fastboot=1 --opt acpi_backlight=vendor"
+	
+	cat > /mnt/etc/systemd/system/mkunifiedimage.service <<-EOD
+	[Unit]
+	Description=Make unified kernel image
+	After=local-fs.target
+	ConditionPathExists=!/boot/EFI/Linux/archlinux-systemd.efi
+	
+	[Service]
+	Type=oneshot
+	RemainAfterExit=true
+	ExecStart=-/root/arch-linux-install/boot/mkinitcpio.sh --reboot --resume PARTLABEL=swap $microcode $opts PARTLABEL=archlinux
+	StandardOutput=inherit
+	StandardError=journal+console
+	
+	[Install]
+	WantedBy=graphical.target
 EOD
-arch-chroot /mnt systemctl enable mkunifiedimage.service
-
-cat <<EOD
-
-------------------------------
-please add a user by running:
-arch-chroot /mnt
-useradd -m -G wheel,uucp <USER>
-passwd <USER>
-exit
-
+	arch-chroot /mnt systemctl enable mkunifiedimage.service
+	
+	cat <<-EOD
+	
+	------------------------------
+	please add a user by running:
+	arch-chroot /mnt
+	useradd -m -G wheel,uucp <USER>
+	passwd <USER>
+	exit
+	
 EOD
-
-cat <<EOD
-
--------------------------
-to finish the install run:
-umount -R /mnt
-reboot
-
+	
+	cat <<-EOD
+	
+	-------------------------
+	to finish the install run:
+	umount -R /mnt
+	reboot
+	
 EOD
+}
 
+main "$@"
 #uuidroot=$(blkid |awk -F\" '/sda3/ { print $10 }')
 #uuidswap=$(blkid |awk -F\" '/sda2/ { print $6 }')
 #efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$uuidroot resume=PARTUUID=$uuidswap rw quiet i915.fastboot=1 consoleblank=1 initrd=\intel-ucode.img initrd=\initramfs-linux.img"
