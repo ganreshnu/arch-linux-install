4a3c1e057902b69a0c565ca3d44fc73786f151f3
Ganreshnu <ganreshnu@gmail.com>
2023-01-14 00:41:51 -0600

    changing the mirrorlist command

--- install2.sh
+++ install2.sh
@@ -256,7 +256,7 @@ main() {
 	[[ $go =~ y|Y ]] && echo || return 1
 
 	msg install 4 "updating the pacman mirrorlist"
-	reflector --save /etc/pacman.d/mirrorlist --country US --latest 5 --protocol 'https'
+	reflector --save /etc/pacman.d/mirrorlist --country US --age 1 --score 6 --fastest 3 --protocol 'https'
 
 	# bootstrap the install
 	msg install 4 "installing the packages"
