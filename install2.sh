a832e6b57f3f40a85d8e4527719ab2e2c72ed23b
Ganreshnu <ganreshnu@gmail.com>
2023-01-18 01:32:22 -0600

    testing mkfstab

--- install2.sh
+++ install2.sh
@@ -223,8 +223,9 @@ mkfstab() {
 	local mounts=$(findmnt --submounts --nofsroot --noheadings --output SOURCE,TARGET,FSTYPE,OPTIONS,FSROOT --canonicalize --evaluate --raw --notruncate $mountpoint)
 	echo "$mounts" | while read -r src target fstype opts fsroot; do
 		target="${target#$mountpoint}"
+		[[ $target == '/boot' ]] && continue
 		printf '%s %s %s %s %i %i\n' \
-			"$(blkid -o value -s UUID "$src")" \
+			"UUID=$(blkid -o value -s UUID "$src")" \
 			"${target:-/}" \
 			"$fstype" \
 			"$opts" \
